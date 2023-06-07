861c1b37d0a18e893551d2befb116a06936a23ba
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2023-06-07 08:04:23 +0530

    chore: updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,39 @@
-import RyzolveHeader from "components/ryzolve/RyzolveHeader";
-import React from "react";
-import { Hero2 } from "components/blocks/hero";
-import { processList5 } from "data/process";
-import Image from "next/image";
-import { Services12, Services2 } from "components/blocks/services";
-import { Testimonial4 } from "components/blocks/testimonial";
-import { Process1 } from "components/blocks/process";
-import { Pricing4 } from "components/blocks/pricing";
-import { Contact10 } from "components/blocks/contact";
-import { Footer3 } from "components/blocks/footer";
-import { About14 } from "components/blocks/about";
+import RyzolveHeader from 'components/ryzolve/RyzolveHeader';
+import React from 'react';
+import { Hero2 } from 'components/blocks/hero';
+import { processList5 } from 'data/process';
+import Image from 'next/image';
+import { Services12, Services2 } from 'components/blocks/services';
+import { Testimonial4 } from 'components/blocks/testimonial';
+import { Process1 } from 'components/blocks/process';
+import { Pricing4 } from 'components/blocks/pricing';
+import { Contact10 } from 'components/blocks/contact';
+import { Footer3 } from 'components/blocks/footer';
+import { About14 } from 'components/blocks/about';
 
 const ryzolve = () => {
   return (
     <div>
-      <header className="wrapper bg-gray">
+      <header className='wrapper bg-gray'>
         <RyzolveHeader />
       </header>
-      <section className="content-wrapper wrapper">
-        <section className="p-12 wrapper bg-gray">
+      <section className='content-wrapper wrapper'>
+        <section className='p-12 wrapper bg-gray'>
           <Hero2 />
         </section>
-        <div className="row gx-lg-8 gx-xl-12 process-wrapper text-center mt-9 px-6">
-          {processList5.map(({ title, subtitle, Icon, color }) => (
-            <div key={title} className="col-md-4">
+        <div className='row gx-lg-8 gx-xl-12 process-wrapper text-center mt-9 px-6'>
+          {processList5.map(({ title, Icon, color }) => (
+            <div key={title} className='col-md-4'>
               {<Icon className={`icon-svg-md text-${color} mb-3`} />}
-              <h4 className="mb-1">{title}</h4>
-              <p>{subtitle}</p>
+              <h4 className='mb-1'>{title}</h4>
+              {/* <p>{subtitle}</p> */}
             </div>
           ))}
         </div>
-        <section className="py-12">
+        <section className='py-12'>
           <Services2 />
         </section>
-        <section className="p-12">
+        <section className='p-12'>
           <Services12 />
         </section>
         {/* <BlogCard2
@@ -62,11 +62,11 @@ const ryzolve = () => {
         /> */}
         {/* <Services14 /> */}
         <Testimonial4 />
-        <section className="p-12">
+        <section className='p-12'>
           <Process1 />
         </section>
         <Pricing4 />
-        <section className="p-12">
+        <section className='p-12'>
           <About14 />
         </section>
         <Contact10 />
