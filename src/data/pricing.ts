da12762221ca1f8e2dcc9180bdc8f74653c314d8
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2023-06-07 08:12:55 +0530

    chore: quick fix

--- src/data/pricing.ts
+++ src/data/pricing.ts
@@ -1,32 +1,32 @@
 // -------- icons -------- //
-import Home from "icons/lineal/Home";
-import BriefcaseTwo from "icons/lineal/BriefcaseTwo";
-import ShoppingBasket from "icons/lineal/ShoppingBasket";
+import Home from 'icons/lineal/Home';
+import BriefcaseTwo from 'icons/lineal/BriefcaseTwo';
+import ShoppingBasket from 'icons/lineal/ShoppingBasket';
 
 // used in the pricing-1, pricing-3 block
 export const pricingList1 = [
   {
     monthlyPrice: 19,
     yearlyPrice: 199,
-    planName: "Premium",
+    planName: 'Premium',
     features: [
-      "5 Projects",
-      "100K API Access",
-      "200MB Storage",
-      "Weekly Reports",
-      "7/24 Support",
+      '5 Projects',
+      '100K API Access',
+      '200MB Storage',
+      'Weekly Reports',
+      '7/24 Support',
     ],
   },
   {
     monthlyPrice: 49,
     yearlyPrice: 499,
-    planName: "Corporate",
+    planName: 'Corporate',
     features: [
-      "20 Projects",
-      "300K API Access",
-      "500MB Storage",
-      "Weekly Reports",
-      "7/24 Support",
+      '20 Projects',
+      '300K API Access',
+      '500MB Storage',
+      'Weekly Reports',
+      '7/24 Support',
     ],
   },
 ];
@@ -37,30 +37,30 @@ export const pricingList2 = [
     monthlyPrice: 9,
     yearlyPrice: 99,
     Icon: ShoppingBasket,
-    planName: "Start Up",
-    features: ["Document Management", "Compliance", "Claims & Billing"],
+    planName: 'Start Up',
+    features: ['Document Management', 'Compliance', 'Claims & Billing'],
   },
   {
     Icon: Home,
     monthlyPrice: 19,
     yearlyPrice: 199,
-    planName: "Small Business",
-    features: ["Document Management", "Compliance", "Claims & Billing"],
+    planName: 'Small Business',
+    features: ['Document Management', 'Compliance', 'Claims & Billing'],
   },
   {
     monthlyPrice: 49,
     yearlyPrice: 499,
     Icon: BriefcaseTwo,
-    planName: "Large Business",
-    features: ["Document Management", "Compliance", "Claims & Billing"],
+    planName: 'Large Business',
+    features: ['Document Management', 'Compliance', 'Claims & Billing'],
   },
-  {
-    monthlyPrice: 99,
-    yearlyPrice: 999,
-    Icon: BriefcaseTwo,
-    planName: "Enterprise",
-    features: ["Document Management", "Compliance", "Claims & Billing"],
-  },
+  // {
+  //   monthlyPrice: 99,
+  //   yearlyPrice: 999,
+  //   Icon: BriefcaseTwo,
+  //   planName: "Enterprise",
+  //   features: ["Document Management", "Compliance", "Claims & Billing"],
+  // },
 ];
 
 // used in the pricing-8 block
@@ -68,37 +68,37 @@ export const pricingList3 = [
   {
     monthlyPrice: 9,
     yearlyPrice: 99,
-    planName: "Basic",
+    planName: 'Basic',
     features: [
-      "1 Project",
-      "100K API Access",
-      "100MB Storage",
-      "Weekly Reports",
-      "7/24 Support",
+      '1 Project',
+      '100K API Access',
+      '100MB Storage',
+      'Weekly Reports',
+      '7/24 Support',
     ],
   },
   {
     monthlyPrice: 19,
     yearlyPrice: 199,
-    planName: "Premium",
+    planName: 'Premium',
     features: [
-      "5 Projects",
-      "100K API Access",
-      "200MB Storage",
-      "Weekly Reports",
-      "7/24 Support",
+      '5 Projects',
+      '100K API Access',
+      '200MB Storage',
+      'Weekly Reports',
+      '7/24 Support',
     ],
   },
   {
     monthlyPrice: 49,
     yearlyPrice: 499,
-    planName: "Corporate",
+    planName: 'Corporate',
     features: [
-      "20 Projects",
-      "300K API Access",
-      "500MB Storage",
-      "Weekly Reports",
-      "7/24 Support",
+      '20 Projects',
+      '300K API Access',
+      '500MB Storage',
+      'Weekly Reports',
+      '7/24 Support',
     ],
   },
 ];
