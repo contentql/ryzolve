b68d3b989338daa0b6a0b1619910ad15b6a6c9b5
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2023-06-28 01:08:31 +0530

    chore: fixing Hero section

--- pages/claims-and-bills/index.tsx
+++ pages/claims-and-bills/index.tsx
@@ -1,27 +1,30 @@
-import RyzolveHeader from "components/ryzolve/RyzolveHeader";
-import React from "react";
-import { Hero10 } from "components/blocks/hero";
-import { Services2, Services21 } from "components/blocks/services";
-import { Testimonial4 } from "components/blocks/testimonial";
-import { Contact10 } from "components/blocks/banner/contact";
-import { Footer3 } from "components/blocks/footer";
-import { About14 } from "components/blocks/about";
+import RyzolveHeader from 'components/ryzolve/RyzolveHeader';
+import React from 'react';
+import { Hero10 } from 'components/blocks/hero';
+import { Services2, Services21 } from 'components/blocks/services';
+import { Testimonial4 } from 'components/blocks/testimonial';
+import { Contact10 } from 'components/blocks/banner/contact';
+import { Footer3 } from 'components/blocks/footer';
+import { About14 } from 'components/blocks/about';
 
 const index = () => {
   return (
     <div>
-      <header className="wrapper bg-gray">
+      <header className='wrapper bg-gray'>
         <RyzolveHeader />
       </header>
-      <section className="content-wrapper wrapper">
-        <section className="p-12 wrapper bg-gray">
-          <Hero10 />
+      <section className='content-wrapper wrapper'>
+        <section className='p-12 wrapper bg-gray'>
+          <Hero10
+            title='Managing Paper Work Made Easier'
+            subtitle='Electronic Document Management Helping you increase Efficiency'
+          />
         </section>
 
         {/* Services Section */}
 
-        <section className="wrapper bg-light ">
-          <section className="container pt-2 pb-md-2">
+        <section className='wrapper bg-light '>
+          <section className='container pt-2 pb-md-2'>
             {/* ========== services section ========== */}
             <Services21 />
           </section>
@@ -30,13 +33,13 @@ const index = () => {
         <section>
           <Services2 />
         </section>
-        <section className="mb-md-12">
+        <section className='mb-md-12'>
           <Testimonial4 />
         </section>
         {/* <section className='px-12 pt-12'>
           <Process1 />
         </section> */}
-        <section className="px-12  bg-gray pb-12">
+        <section className='px-12  bg-gray pb-12'>
           <About14 />
         </section>
         <Contact10 />
--- pages/compliance-regulation/index.tsx
+++ pages/compliance-regulation/index.tsx
@@ -1,27 +1,30 @@
-import RyzolveHeader from "components/ryzolve/RyzolveHeader";
-import React from "react";
-import { Hero10 } from "components/blocks/hero";
-import { Services2, Services21 } from "components/blocks/services";
-import { Testimonial4 } from "components/blocks/testimonial";
-import { Contact10 } from "components/blocks/banner/contact";
-import { Footer3 } from "components/blocks/footer";
-import { About14 } from "components/blocks/about";
+import RyzolveHeader from 'components/ryzolve/RyzolveHeader';
+import React from 'react';
+import { Hero10 } from 'components/blocks/hero';
+import { Services2, Services21 } from 'components/blocks/services';
+import { Testimonial4 } from 'components/blocks/testimonial';
+import { Contact10 } from 'components/blocks/banner/contact';
+import { Footer3 } from 'components/blocks/footer';
+import { About14 } from 'components/blocks/about';
 
 const index = () => {
   return (
     <div>
-      <header className="wrapper bg-gray">
+      <header className='wrapper bg-gray'>
         <RyzolveHeader />
       </header>
-      <section className="content-wrapper wrapper">
-        <section className="p-12 wrapper bg-gray">
-          <Hero10 />
+      <section className='content-wrapper wrapper'>
+        <section className='p-12 wrapper bg-gray'>
+          <Hero10
+            title='Stay Compliant'
+            subtitle='Compliance that meets all government requirements, providing automation and peace of mind'
+          />
         </section>
 
         {/* Services Section */}
 
-        <section className="wrapper bg-light ">
-          <section className="container pt-2 pb-md-2">
+        <section className='wrapper bg-light '>
+          <section className='container pt-2 pb-md-2'>
             {/* ========== services section ========== */}
             <Services21 />
           </section>
@@ -30,13 +33,13 @@ const index = () => {
         <section>
           <Services2 />
         </section>
-        <section className="mb-md-12">
+        <section className='mb-md-12'>
           <Testimonial4 />
         </section>
         {/* <section className='px-12 pt-12'>
           <Process1 />
         </section> */}
-        <section className="px-12  bg-gray pb-12">
+        <section className='px-12  bg-gray pb-12'>
           <About14 />
         </section>
         <section>
