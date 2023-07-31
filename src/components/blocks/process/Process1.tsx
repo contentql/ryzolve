043c24ba8f7655ff04e1f96a4950e05be1c84fb7
EswarBenarjee <eswar.bane2002.india@gmail.com>
2023-07-31 15:40:40 +0530

    chore: Added Book a demo button

--- src/components/blocks/process/Process1.tsx
+++ src/components/blocks/process/Process1.tsx
@@ -1,43 +1,45 @@
-import { FC } from 'react';
-import NextLink from 'components/reuseable/links/NextLink';
-import { ProcessList1 } from 'components/reuseable/process-list';
+import { FC } from "react";
+import NextLink from "components/reuseable/links/NextLink";
+import { ProcessList1 } from "components/reuseable/process-list";
 // -------- data -------- //
-import { processList1 } from 'data/process';
+import { processList1 } from "data/process";
 
 const Process1: FC = () => {
   return (
-    <div className='row gx-md- gx-xl-12 gy-10 mb-4 mb-md-8 px-5 align-items-center'>
-      <div className='col-lg-6 order-lg-2'>
+    <div className="row gx-md- gx-xl-12 gy-10 mb-4 mb-md-8 px-5 align-items-center">
+      <div className="col-lg-6 order-lg-2">
         {processList1.map((item) => {
           return <ProcessList1 shadow {...item} key={item.no} />;
         })}
+
+        <div className="p-8 d-flex justify-content-center">
+          <NextLink
+            href="#"
+            title="Book a demo"
+            className="btn btn-primary rounded-pill mb-0"
+          />
+        </div>
       </div>
 
-      <div className='col-lg-6'>
-        <h2 className='fs-16 text-uppercase text-muted mb-3'>How it Works</h2>
-        <h3 className='display-4 mb-5'>
+      <div className="col-lg-6">
+        <h2 className="fs-16 text-uppercase text-muted mb-3">How it Works</h2>
+        <h3 className="display-4 mb-5">
           Managing Claims and Paperwork Should be Easier.
         </h3>
 
         <figure
           style={{
-            maxHeight: '500px',
-            overflow: 'hidden',
+            maxHeight: "500px",
+            overflow: "hidden",
           }}
         >
           <img
-            alt='our strategy'
-            src='/img/photos/our-strategy.svg'
-            className='img-fluid'
+            alt="our strategy"
+            src="/img/photos/our-strategy.svg"
+            className="img-fluid"
             // srcSet='/img/illustrations/i8@2x.png 2x'
           />
         </figure>
-
-        {/* <NextLink
-          href='#'
-          title='Learn More'
-          className='btn btn-primary rounded-pill mb-0'
-        /> */}
       </div>
     </div>
   );
