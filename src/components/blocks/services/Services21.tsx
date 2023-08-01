c0eb72776fed7d43bbd20cd5f8bf316a4b80f36d
manoj karajada <48806551+julydeath@users.noreply.github.com>
2023-08-01 16:52:42 +0530

    chore: card rotate on hover (#9)

--- src/components/blocks/services/Services21.tsx
+++ src/components/blocks/services/Services21.tsx
@@ -1,13 +1,13 @@
-import { FC } from 'react';
-import { ServiceCard1 } from 'components/reuseable/service-cards';
+import { FC } from "react";
+import { ServiceCard1 } from "components/reuseable/service-cards";
 // -------- data -------- //
-import { serviceList11 } from 'data/service';
+import { serviceList11 } from "data/service";
 
 const Services21: FC = () => {
   return (
     <div className="row gx-md-5 gy-5 mt-n19 mb-14 mb-md-17 ">
       {serviceList11.map(({ id, Icon, ...item }) => (
-        <ServiceCard1 {...item} key={id} linkUrl="#" Icon={Icon} />
+        <ServiceCard1 {...item} key={id} Icon={Icon} />
       ))}
     </div>
   );
