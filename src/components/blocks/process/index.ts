df8a9936871ad4168ae71ef8161d5bd04d3d5c1d
julydeath <manojkarajada.mk@gmail.com>
2024-02-28 13:49:48 +0530

    chore : contact and about pages

--- src/components/blocks/process/index.ts
+++ src/components/blocks/process/index.ts
@@ -1,4 +1,5 @@
 import Process1 from "./Process1";
 import Process3 from "./Process3";
+import Process8 from "./Process8";
 
-export { Process1, Process3 };
+export { Process1, Process3, Process8 };
