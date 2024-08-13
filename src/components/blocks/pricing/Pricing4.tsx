948eb6e67567021d3a5bc335dcf4164b4f6f9c13
julydeath <manojkarajada.mk@gmail.com>
2024-08-14 04:10:55 +0530

    chore : pricing max 3

--- src/components/blocks/pricing/Pricing4.tsx
+++ src/components/blocks/pricing/Pricing4.tsx
@@ -5,6 +5,7 @@ import Pricing2 from "./Pricing2";
 
 const Pricing4: FC = () => {
   const { data } = useQuery({ queryKey: ["pricing"], queryFn: getPricingData });
+  console.log("data", data);
   return (
     <div>
       <div className="wrapper bg-soft-primary">
@@ -12,7 +13,7 @@ const Pricing4: FC = () => {
           <div className="row">
             <div className="col-lg-10 col-xl-9 col-xxl-8 mx-auto">
               <h3 className="display-4 mb-15 mb-md-6 px-lg-10 custom-description-text">
-                {data?.pricing.title}
+                {data?.data.pricing2.data.attributes.title}
               </h3>
             </div>
           </div>
@@ -22,7 +23,7 @@ const Pricing4: FC = () => {
       {/* pricing list section */}
       <div className="wrapper bg-light">
         <div className="container pt-14 pt-md-14 pb-8 pb-md-8">
-          <Pricing2 planData={data?.pricing.plans} />
+          <Pricing2 planData={data?.data.pricing2.data.attributes.plans} />
         </div>
       </div>
     </div>
--- src/queries/pricing/index.ts
+++ src/queries/pricing/index.ts
@@ -3,32 +3,29 @@ import fetchData from "utils/fetchData";
 export const getPricingData = async () => {
   const data = await fetchData(
     `
-    query{
-        pricing{
-        data{
-            attributes{
-            pricing{
-                title
-                plans{
-                id
-                monthly_price
-                yearly_price
-                plan_name
-                plan_features{
-                    id
-                    item
-                }
-                }
-            }
-            }
+query {
+	pricing2{
+    data{
+      attributes{
+        title
+        plans{
+          monthly_price
+          yearly_price
+          plan_name
+          plan_features{
+            id
+            item
+          }
         }
-        }
+      }
     }
+  }
+}
     
   `,
     {
       variables: {},
     }
   );
-  return data.data.pricing.data.attributes;
+  return data;
 };
