1ccb03cce272fd59aa0abd93d452c5365ae73ba7
charanm927 <charanm927@gmail.com>
2025-05-05 17:55:07 +0530

    chore: added optional

--- src/components/blocks/pricing/Pricing4.tsx
+++ src/components/blocks/pricing/Pricing4.tsx
@@ -23,7 +23,7 @@ const Pricing4: FC = () => {
       {/* pricing list section */}
       <div className="wrapper bg-light">
         <div className="container pt-14 pt-md-14 pb-8 pb-md-8">
-          <Pricing2 planData={data?.data?.pricing2.data?.attributes?.plans} />
+          <Pricing2 planData={data?.data?.pricing2?.data?.attributes?.plans} />
           {/* <PricingTable planData={data?.data?.pricing2?.data?.attributes?.plans}/> */}
         </div>
       </div>
