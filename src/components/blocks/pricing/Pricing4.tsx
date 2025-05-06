5de023f6d6d5592af9a3edb57be3bf6e08f13c3f
charanm927 <charanm927@gmail.com>
2025-05-06 15:14:55 +0530

    chore: Created pricing table

--- src/components/blocks/pricing/Pricing4.tsx
+++ src/components/blocks/pricing/Pricing4.tsx
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { getPricingData } from "queries/pricing";
 import { FC } from "react";
-import Pricing2 from "./Pricing2";
+// import Pricing2 from "./Pricing2";
+import PricingTable from "./PricingTable";
 
 const Pricing4: FC = () => {
   const { data } = useQuery({ queryKey: ["pricing"], queryFn: getPricingData });
@@ -22,8 +23,8 @@ const Pricing4: FC = () => {
       {/* pricing list section */}
       <div className="wrapper bg-light">
         <div className="container pt-14 pt-md-14 pb-8 pb-md-8">
-          <Pricing2 planData={data?.data?.pricing2?.data?.attributes?.plans} />
-          {/* <PricingTable planData={data?.data?.pricing2?.data?.attributes?.plans}/> */}
+          {/* <Pricing2 planData={data?.data?.pricing2?.data?.attributes?.plans} /> */}
+          <PricingTable planData={data?.data?.pricing2?.data?.attributes?.plans}/>
         </div>
       </div>
     </div>
